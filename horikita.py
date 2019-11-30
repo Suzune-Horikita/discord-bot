20ba4171724b22ffa578c82c1f17da0ea2fb57fe
Suzune-Horikita <58349478+Suzune-Horikita@users.noreply.github.com>
2019-11-30 10:23:30 +0800

    Update horikita.py

--- horikita.py
+++ horikita.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 from random import randint
 
-bot = commands.Bot(command_prefix='\\')
+bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
     print("Bot is Online")
 
-bot.run('NjQ1NDk1Nzk0NTQ3NTU2MzU0.XeHKDw.DODvoH3CsBUK2pHD9t7NKXlOr8c')
+bot.run('')
